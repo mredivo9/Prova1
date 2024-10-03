8b33feabc558ab9363e3a97fbd57c783743f72be
mredivo9 <168040123+mredivo9@users.noreply.github.com>
2024-10-02 21:00:04 -0300

    Add files via upload
    deu branco

---
+++ AtividadeProva/src/atividade2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class atividade2 {
+    public static void main(String[] args) {
+
+                Scanner scanner = new Scanner(System.in);
+
+
+                System.out.println("Bem-vindo ao programa de votação de talentos!");
+                System.out.println("Digite o nome do candidato para votar (ou 'sair' para encerrar):");
+                System.out.print("Seu voto: ");
+                String voto = scanner.nextLine();
+
+                    }
+                }
+
+                System.out.println("Resultados da votação:");
+
+                scanner.close();
+            }
+}
